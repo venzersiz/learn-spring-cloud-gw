bb6fc2c9329f17dc459ab6910750a490c7aef68c
Krevis <venzersiz@gmail.com>
2023-04-26 21:22:46 +0900

    프로젝트 생성

---
+++ src/main/java/xyz/krevis/learn/springcloudgw/LearnSpringCloudGwApplication.java
@@ -0,0 +1,13 @@
+package xyz.krevis.learn.springcloudgw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringCloudGwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSpringCloudGwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/krevis/learn/springcloudgw/LearnSpringCloudGwApplicationTests.java
@@ -0,0 +1,12 @@
+package xyz.krevis.learn.springcloudgw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringCloudGwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
